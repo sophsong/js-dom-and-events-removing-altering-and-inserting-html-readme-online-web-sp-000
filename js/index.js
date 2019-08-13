ae407175a7071c7631d05d3aab91289935f18624
github username <github email address>
2019-08-13 14:14:05 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,11 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+let element = document.createElement('div')
+element.innerHTML = 'Hello, DOM!';
+element.style.backgroundColor = "red"
+
+document.body.appendChild(element)
+
+let anotherElement = document.createElement('ul')
+anotherElement.innerText = "hi"
+ul.remove()
